56b54698f571ca86fab0ae09bb7d48d139597a66
Lukas Müller <lukas@seeven.ch>
2023-09-12 16:36:29 +0200

    123

---
+++ content/day06/01/sketch.js
@@ -0,0 +1,66 @@
+function setup() {
+  createCanvas(1000, 1000, WEBGL);
+  rectMode(CORNER)
+
+}
+
+let img;
+function preload() {
+  img = loadImage('cup.jpg');
+  print(img.width, img.height)
+}
+
+function draw() {
+  background(0)
+  orbitControl()
+  translate( -width /2 , -height / 2)
+
+
+  // image(img, 0,0,width,height)
+  imageCreate()
+}
+
+
+function imageCreate(){
+  let w = width / img.width
+  let h = height / img.height
+  let size = 8
+
+
+  img.loadPixels()
+  for (let i = 0; i < img.width; i+= size){
+    for(let j =0;  j < img.height; j+= size){
+      const pixelIndex = (i+j*img.width)*4
+      const r = img.pixels[pixelIndex + 0]
+      const g = img.pixels[pixelIndex + 1]
+      const b = img.pixels[pixelIndex + 2]
+      const avg = (r + g + b) / 3
+      noStroke()
+         
+      // circle(i*w, j*h, size)
+      // fill(255-2,g,1)
+
+
+      push()
+        fill(avg)
+        translate(0,0,-r)
+        circle(i*w+1, j*h+1, size)
+      pop()
+
+
+
+      
+   
+
+   
+
+  }
+ }
+}
+
+function keyPressed() {
+  // this will download the first 5 seconds of the animation!
+  if (key === 's') {
+    saveGif('mySketch', 5);
+  }
+}
---
+++ day6.md
@@ -0,0 +1,14 @@
+
+# Day 06
+## Introduction 
+Topic of the fourth day is "Pixels". 
+
+
+## Concept
+
+
+#### Variations
+[Image Processing](https://idmnyu.github.io/p5.js-image/index.html) \
+[Generative Gestaltung](http://www.generative-gestaltung.de/2/) \
+
+https://p5js.org/reference/#/p5/get
